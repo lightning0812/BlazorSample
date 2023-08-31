1cd7d43441f848503605fbd56394462c78734c91
lightning0812 <okazakilightning@gmail.com>
2023-08-31 09:31:09 +0900

    DynamicComponentとfavicon実装サンプル追加

---
+++ Blazor_DynamicComponent/Blazor_DynamicComponent/Model/ComponentMetadata.cs
@@ -0,0 +1,19 @@
+﻿namespace Blazor_DynamicComponent.Model
+{
+    /// <summary>
+    /// コンポーネントの情報クラス
+    /// </summary>
+    public class ComponentMetadata
+    {
+        /// <summary>
+        /// UIに表示する名前
+        /// </summary>
+        public string? Name { get; set; }
+
+        /// <summary>
+        /// コンポーネントに渡すパラメータ
+        /// </summary>
+        public Dictionary<string, object> Parameters { get; set; } =
+            new Dictionary<string, object>();
+    }
+}
