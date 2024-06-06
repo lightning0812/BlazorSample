bc69ed6ff26500c89e9b31853c21db21bfdd0c25
lightning0812 <okazakilightning@gmail.com>
2024-06-06 10:48:46 +0900

    javascriptから.NETを呼び出すサンプルを作成

---
+++ BlazorJavaScriptCallDotnet/BlazorJavaScriptCallDotnet/wwwroot/js/script.js
@@ -0,0 +1,9 @@
+﻿async function callDotNetMethod () {
+    // 非同期で .NETメソッドを呼び出し、結果を取得する例
+    const result = await DotNet.invokeMethodAsync('BlazorJavaScriptCallDotnet', 'GetMessage');
+    // 取得結果である「.NET -> JavaScript へのメッセージ」がコンソールへ表示されます
+    console.log(result); 
+
+    // 非同期で .NETメソッドにデータ（引数）を渡して呼び出す例
+    await DotNet.invokeMethodAsync('BlazorJavaScriptCallDotnet', 'UpdateMessage', 'JavaScript -> .NET へのメッセージ');
+};
