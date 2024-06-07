ccbe8c87de6d4148594cf0c8c162cc71ce71c0b1
lightning0812 <86187623+lightning0812@users.noreply.github.com>
2024-06-07 17:03:47 +0900

    SQLite接続サンプル追加

---
+++ Blazor_SQLite/Blazor_SQLite/Data/TestDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Blazor_SQLite.Entities;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blazor_SQLite.Data
+{
+    public class TestDbContext : DbContext
+    {
+        public DbSet<Weapon> Weapons { get; set; }
+
+        private readonly IWebHostEnvironment _env;
+
+        public TestDbContext(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var filePath = Path.Combine(_env.WebRootPath, "test.db");
+            var connectionString = new SqliteConnectionStringBuilder { DataSource = filePath }.ToString();
+            optionsBuilder.UseSqlite(new SqliteConnection(connectionString));
+        }
+    }
+}
---
+++ Blazor_SQLite/Blazor_SQLite/Entities/Weapon.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Blazor_SQLite.Entities
+{
+    /// <summary>
+    /// 武器テーブルのエンティティクラス
+    /// </summary>
+    [Table("Weapons")]
+    public class Weapon
+    {
+        /// <summary>
+        /// 識別子
+        /// </summary>
+        [Key]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// 名前
+        /// </summary>
+        [Required]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 武器説明
+        /// </summary>
+        [Required]
+        public string Description { get; set; }
+
+        /// <summary>
+        /// 攻撃力
+        /// </summary>
+        [Required]
+        public int AttackPower { get; set; }
+    }
+
+}
