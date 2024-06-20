08f4a2a22fda0125349a4a296c6737467a688692
lightning0812 <okazakilightning@gmail.com>
2024-06-20 15:42:14 +0900

    不要なコードを削除

--- Blazor_UploadAndDownload/Blazor_UploadAndDownload/wwwroot/js/functions.js
+++ Blazor_UploadAndDownload/Blazor_UploadAndDownload/wwwroot/js/functions.js
@@ -1,21 +1,18 @@
-﻿// ファイルのバイナリ版（BOMあり）
-function downloadFromByteArray(data) {
-    // Blob 型のオブジェクトで包む
+﻿// ファイルダウンロード
+function downloadFile(data) {
+    // Blob作成
     const blob = new Blob([data.byteArray], { type: data.contentType });
-    // この Blob オブジェクトにリンクした "オブジェクト URL" を生成
-    const url = URL.createObjectURL(blob);
-    // 先の節で実装した、指定された URL からのダウンロードを開始する
-    // JavaScript 関数を呼び出して、ダウンロードを開始    downloadFromUrl({ url: url, fileName: options.fileName });
-    downloadFromUrl(url, data.fileName);
-    // 最後に不要リソースの後始末
-    URL.revokeObjectURL(url);
-}
 
-//ファイルダウンロード
-function downloadFromUrl(url, fileName) {
+    // ダウンロード可能なリンクを作成
+    const url = URL.createObjectURL(blob);
+
+    // URL からのダウンロードを開始する
     const anchorElement = document.createElement('a');
     anchorElement.href = url;
-    anchorElement.download = fileName ?? '';
+    anchorElement.download = data.fileName ?? '';
     anchorElement.click();
     anchorElement.remove();
+
+    // URLの後始末
+    URL.revokeObjectURL(url);
 }
