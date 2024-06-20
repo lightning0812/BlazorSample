363658f940efc3da44c686ecfcdd603ff4613c6d
lightning0812 <okazakilightning@gmail.com>
2024-06-20 11:09:31 +0900

    ファイルアップロード＆ダウンロードサンプル追加

---
+++ Blazor_UploadAndDownload/Blazor_UploadAndDownload/wwwroot/js/functions.js
@@ -0,0 +1,21 @@
+﻿// ファイルのバイナリ版（BOMあり）
+function downloadFromByteArray(data) {
+    // Blob 型のオブジェクトで包む
+    const blob = new Blob([data.byteArray], { type: data.contentType });
+    // この Blob オブジェクトにリンクした "オブジェクト URL" を生成
+    const url = URL.createObjectURL(blob);
+    // 先の節で実装した、指定された URL からのダウンロードを開始する
+    // JavaScript 関数を呼び出して、ダウンロードを開始    downloadFromUrl({ url: url, fileName: options.fileName });
+    downloadFromUrl(url, data.fileName);
+    // 最後に不要リソースの後始末
+    URL.revokeObjectURL(url);
+}
+
+//ファイルダウンロード
+function downloadFromUrl(url, fileName) {
+    const anchorElement = document.createElement('a');
+    anchorElement.href = url;
+    anchorElement.download = fileName ?? '';
+    anchorElement.click();
+    anchorElement.remove();
+}
